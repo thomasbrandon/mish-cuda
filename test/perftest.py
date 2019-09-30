ac921450cb15d56ed3e95f090186d8b7c5bd23e1
Thomas Brandon <tbrandonau@gmail.com>
2019-10-01 03:49:43 +1000

    Perftest all data types

--- test/perftest.py
+++ test/perftest.py
@@ -48,22 +48,31 @@ if __name__ == '__main__':
     parser.add_argument('-w', '--warmup', type=int, default=10)
     parser.add_argument('-s', '--size', default="(16,10,256,256)")
     parser.add_argument('-b', '--baseline', action='store_true')
+    parser.add_argument('-t', '--type', default='all')
     args = parser.parse_args()
+    if args.type == 'all': dtypes = [torch.float16, torch.float32, torch.float64]
+    else:
+        if not hasattr(torch, args.type): exit("Invalid data type, expected torch type or 'all', got {args.type}")
+        dtypes = [getattr(torch, args.type)]
     dev = torch.device(type='cuda', index=args.device)
     sz_str = args.size.replace(' ','')
     if not re.match(r"[\(\[]\d+(,\d+)*[\)\]]", sz_str):
         exit("Badly formatted size, should be a list or tuple such as \"(1,2,3)\".")
     sz = list(map(int, sz_str[1:-1].split(',')))
-    inp = torch.randn(*sz, dtype=torch.float32, device=dev)
-
     print(f"Profiling over {args.n_repeat} runs after {args.warmup} warmup runs.")
     print(f"Profiling on {torch.cuda.get_device_name(dev)}")
-    timings = []
-    funcs = {}
-    if args.baseline: funcs.update(relu=torch.nn.functional.relu, softplus=torch.nn.functional.softplus, mish_pt=mish_pt)
-    funcs['mish_cuda'] = MishCudaFunction.apply
-    max_name = max(map(len, funcs.keys())) + 6
-    for (name,func) in funcs.items():
-        fwd_times,bwd_times = profile(func, inp, args.n_repeat, args.warmup)
-        print((name+'_fwd:').ljust(max_name) + display_times(fwd_times))
-        print((name+'_bwd:').ljust(max_name) + display_times(bwd_times))
+    for dtype in dtypes:
+        if len(dtypes) > 1:
+            print(f"Testing on {dtype}:")
+            ind = ' '
+        else: ind = ''
+        inp = torch.randn(*sz, dtype=dtype, device=dev)
+        timings = []
+        funcs = {}
+        if args.baseline: funcs.update(relu=torch.nn.functional.relu, softplus=torch.nn.functional.softplus, mish_pt=mish_pt)
+        funcs['mish_cuda'] = MishCudaFunction.apply
+        max_name = max(map(len, funcs.keys())) + 6
+        for (name,func) in funcs.items():
+            fwd_times,bwd_times = profile(func, inp, args.n_repeat, args.warmup)
+            print(ind+(name+'_fwd:').ljust(max_name) + display_times(fwd_times))
+            print(ind+(name+'_bwd:').ljust(max_name) + display_times(bwd_times))
